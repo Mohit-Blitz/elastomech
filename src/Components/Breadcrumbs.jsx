3b622e0557bcaac8054483063a48d524d8c4ca1d
Mohit Kumar <mohit.kumar@indrones.com>
2024-12-21 15:26:37 +0530

    Product

--- src/Components/Breadcrumbs.jsx
+++ src/Components/Breadcrumbs.jsx
@@ -1,17 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Breadcrumbs = ({ page_header_details }) => {
-    return (
-        <div
-            className="rs-breadcrumbs img1"
-        >
-            <div className="">
-                <div className="breadcrumbs-inner">
-                    <h1 className="page-title">{page_header_details.header_name}</h1>
-                    {/* <span className="sub-text break-words">
-                        {page_header_details.header_dec}
-                    </span> */}
-                    <ul
+  return (
+    <div className="rs-breadcrumbs img1">
+      <div className="">
+        <div className="breadcrumbs-inner">
+          <h1 className="page-title">{page_header_details.header_name}</h1>
+          {/* <ul
                         className="breadcrumbs-area"
                     >
                         <li title="Go to home">
@@ -20,11 +15,11 @@ const Breadcrumbs = ({ page_header_details }) => {
                             </a>
                         </li>
                         <li>{page_header_details.header_current_page_name}</li>
-                    </ul>
-                </div>
-            </div>
+            </ul> */}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Breadcrumbs;
